90dea5b51db6b064c732fce299288f995b50dde9
caiqiang <caiqiang@kuaijiankang.com>
2018-06-23 17:26:33 +0800

    添加“比例拼图”demo

---
+++ iOS_Demo/Demos/Base/CQBaseModel.h
@@ -0,0 +1,13 @@
+//
+//  CQBaseModel.h
+//  iOS_Demo
+//
+//  Created by 蔡强 on 2018/6/23.
+//  Copyright © 2018年 蔡强. All rights reserved.
+//
+
+#import <JSONModel/JSONModel.h>
+
+@interface CQBaseModel : JSONModel
+
+@end
---
+++ iOS_Demo/主页/HomeViewController.m
@@ -0,0 +1,65 @@
+//
+//  HomeViewController.m
+//  iOS_Demo
+//
+//  Created by 蔡强 on 2018/6/23.
+//  Copyright © 2018年 蔡强. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+static NSString * const HomeCellReuseID = @"HomeCellReuseID";
+
+@interface HomeViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, strong) NSArray *dataArray;
+
+@end
+
+@implementation HomeViewController
+
+#pragma mark - Lazy Load
+
+- (NSArray *)dataArray {
+    if (!_dataArray) {
+        _dataArray = @[@{@"demo_name" : @"比例拼图", @"controller_name" : @"CQJigsawViewController"}
+                       ];
+    }
+    return _dataArray;
+}
+
+#pragma mark - Life Circle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.title = @"demos";
+    
+    UITableView *tableView = [[UITableView alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview:tableView];
+    tableView.dataSource = self;
+    tableView.delegate = self;
+    [tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:HomeCellReuseID];
+}
+
+#pragma mark - UITableView DataSource && Delegate
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.dataArray.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:HomeCellReuseID];
+    NSDictionary *demoDict = self.dataArray[indexPath.row];
+    cell.textLabel.text = demoDict[@"demo_name"];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSDictionary *demoDict = self.dataArray[indexPath.row];
+    UIViewController *vc = [[NSClassFromString(demoDict[@"controller_name"]) alloc] init];
+    [self.navigationController pushViewController:vc animated:YES];
+}
+
+@end
