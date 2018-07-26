58e64afa9c47a41e8234736f0e0ae1379a8da63f
caiqiang <caiqiang@kuaijiankang.com>
2018-07-26 19:11:15 +0800

    change project name

--- Pods/Target Support Files/Pods-iOS_Demo/Pods-iOS_Demo-dummy.m
+++ Pods/Target Support Files/Pods-iOS_Demo/Pods-iOS_Demo-dummy.m
@@ -1,5 +1,5 @@
 #import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iOS_storage : NSObject
+@interface PodsDummy_Pods_iOS_Storage : NSObject
 @end
-@implementation PodsDummy_Pods_iOS_storage
+@implementation PodsDummy_Pods_iOS_Storage
 @end
---
+++ iOS_Storage/Main/CQContentViewController.h
@@ -0,0 +1,16 @@
+//
+//  CQContentViewController.h
+//  iOS_Storage
+//
+//  Created by 蔡强 on 2018/7/26.
+//  Copyright © 2018年 蔡强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CQContentViewController : UIViewController
+
+- (instancetype)init NS_UNAVAILABLE;
+- (instancetype)initWithContent:(NSString *)content NS_DESIGNATED_INITIALIZER;;
+
+@end
