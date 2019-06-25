b8bda7a73acf479791b5c870eb4a8a9389dccd3c
caiqiang <caiqiang@kuaijiankang.com>
2019-06-25 23:49:13 +0800

    添加模板方法模式

---
+++ iOS_Storage/Contents/架构与设计模式/设计模式/模板方法模式/CQTemplateMethodViewController.h
@@ -0,0 +1,17 @@
+//
+//  CQTemplateMethodViewController.h
+//  iOS_Storage
+//
+//  Created by caiqiang on 2019/6/25.
+//  Copyright © 2019 蔡强. All rights reserved.
+//
+
+#import "CQBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CQTemplateMethodViewController : CQBaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOS_Storage/Contents/架构与设计模式/设计模式/模板方法模式/CQTemplateMethodViewController.m
@@ -0,0 +1,27 @@
+//
+//  CQTemplateMethodViewController.m
+//  iOS_Storage
+//
+//  Created by caiqiang on 2019/6/25.
+//  Copyright © 2019 蔡强. All rights reserved.
+//
+
+#import "CQTemplateMethodViewController.h"
+
+@interface CQTemplateMethodViewController ()
+
+@end
+
+@implementation CQTemplateMethodViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    // 模板方法模式就是继承和多态的运用
+    // 没什么好讲的
+}
+
+
+
+@end
