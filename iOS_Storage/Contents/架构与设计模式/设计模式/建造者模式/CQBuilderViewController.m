d175d94ae76820a7d9a83aff5c0112a296ec3024
caiqiang <caiqiang@kuaijiankang.com>
2019-06-26 14:29:17 +0800

    添加建造者模式

---
+++ iOS_Storage/Contents/架构与设计模式/设计模式/建造者模式/CQBuilderViewController.m
@@ -0,0 +1,22 @@
+//
+//  CQBuilderViewController.m
+//  iOS_Storage
+//
+//  Created by caiqiang on 2019/6/26.
+//  Copyright © 2019 蔡强. All rights reserved.
+//
+
+#import "CQBuilderViewController.h"
+
+@interface CQBuilderViewController ()
+
+@end
+
+@implementation CQBuilderViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
