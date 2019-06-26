7cc2477d6c8a553f8035cc97a35f120e7392fc3c
caiqiang <caiqiang@kuaijiankang.com>
2019-06-26 11:53:27 +0800

    添加外观模式

---
+++ iOS_Storage/Contents/架构与设计模式/设计模式/外观模式/CQFacadeViewController.h
@@ -0,0 +1,17 @@
+//
+//  CQFacadeViewController.h
+//  iOS_Storage
+//
+//  Created by caiqiang on 2019/6/26.
+//  Copyright © 2019 蔡强. All rights reserved.
+//
+
+#import "CQBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CQFacadeViewController : CQBaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOS_Storage/Contents/架构与设计模式/设计模式/外观模式/CQFacadeViewController.m
@@ -0,0 +1,32 @@
+//
+//  CQFacadeViewController.m
+//  iOS_Storage
+//
+//  Created by caiqiang on 2019/6/26.
+//  Copyright © 2019 蔡强. All rights reserved.
+//
+
+#import "CQFacadeViewController.h"
+
+@interface CQFacadeViewController ()
+
+@end
+
+@implementation CQFacadeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
