b1b9f1e2ce07eb188b6fc43f686d732c8c1ceff3
caiqiang <caiqiang@kuaijiankang.com>
2019-06-26 14:47:17 +0800

    添加观察者模式

---
+++ iOS_Storage/Contents/架构与设计模式/设计模式/观察者模式/CQObserverViewController.h
@@ -0,0 +1,17 @@
+//
+//  CQObserverViewController.h
+//  iOS_Storage
+//
+//  Created by caiqiang on 2019/6/26.
+//  Copyright © 2019 蔡强. All rights reserved.
+//
+
+#import "CQBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CQObserverViewController : CQBaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOS_Storage/Contents/架构与设计模式/设计模式/观察者模式/CQObserverViewController.m
@@ -0,0 +1,32 @@
+//
+//  CQObserverViewController.m
+//  iOS_Storage
+//
+//  Created by caiqiang on 2019/6/26.
+//  Copyright © 2019 蔡强. All rights reserved.
+//
+
+#import "CQObserverViewController.h"
+
+@interface CQObserverViewController ()
+
+@end
+
+@implementation CQObserverViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
