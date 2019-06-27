0ec79e3c5a60d7e6e1101b11146fa846dd7bc0dd
caiqiang <caiqiang@kuaijiankang.com>
2019-06-27 13:16:46 +0800

    添加抽象工厂模式

---
+++ iOS_Storage/Contents/架构与设计模式/设计模式/抽象工厂模式/CQAbstractFactoryViewController.m
@@ -0,0 +1,22 @@
+//
+//  CQAbstractFactoryViewController.m
+//  iOS_Storage
+//
+//  Created by caiqiang on 2019/6/27.
+//  Copyright © 2019 蔡强. All rights reserved.
+//
+
+#import "CQAbstractFactoryViewController.h"
+
+@interface CQAbstractFactoryViewController ()
+
+@end
+
+@implementation CQAbstractFactoryViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
