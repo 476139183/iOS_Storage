1a83641987928fd1b3f88602abc6c430836fae54
caiqiang <caiqiang@kuaijiankang.com>
2019-06-27 16:31:11 +0800

    添加适配器模式

---
+++ iOS_Storage/Contents/架构与设计模式/设计模式/适配器模式/CQAdapterViewController.m
@@ -0,0 +1,23 @@
+//
+//  CQAdapterViewController.m
+//  iOS_Storage
+//
+//  Created by caiqiang on 2019/6/27.
+//  Copyright © 2019 蔡强. All rights reserved.
+//
+
+#import "CQAdapterViewController.h"
+
+@interface CQAdapterViewController ()
+
+@end
+
+@implementation CQAdapterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
