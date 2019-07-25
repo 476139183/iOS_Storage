46905ee7739c39dd7a7827a1fd50e10327280c09
宛峰 蔡 <1351675135@qq.com>
2019-07-25 10:16:51 +0800

    添加自定义button说明

--- iOS_Storage/Contents/Demo/自定义button/Controller/CQCustomButtonController.m
+++ iOS_Storage/Contents/Demo/自定义button/Controller/CQCustomButtonController.m
@@ -17,21 +17,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(90, 90, 200, 60)];
+    [self.view addSubview:label];
+    label.numberOfLines = 0;
+    label.text = @"关键就是继承UIControl，详情可以查看简书文章";
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
