233a71a0ec6568460cdaafa5896d0af8b22e2437
caiqiang <1351675135@qq.com>
2019-10-29 19:14:05 +0800

    add UIStackView demo

--- iOS_Storage/Contents/Demo/UIStackViewDemo/StackViewDemoViewController.swift
+++ iOS_Storage/Contents/Demo/UIStackViewDemo/StackViewDemoViewController.swift
@@ -10,7 +10,43 @@ import UIKit
 
 class StackViewDemoViewController: CQBaseViewController {
     
-    private var stackView: UIStackView! = UIStackView()
+    private lazy var stackView: UIStackView = {
+        let stackView  = UIStackView.init(arrangedSubviews: [redView, greenView, blueView])
+        // item间距
+        stackView.spacing = 30
+        // 水平方向布局
+        stackView.axis = .horizontal
+        // 底部对齐
+        stackView.alignment = .bottom
+        // 等间距
+        stackView.distribution = .equalSpacing
+        return stackView
+    }()
+    
+    private lazy var stackView2: UIStackView = {
+        var imageViewArray: [UIView] = []
+        for _ in 0..<5 {
+            let imageView = UIImageView.init(image: UIImage.init(named: "interesting"))
+            imageView.snp.makeConstraints { (make) in
+                make.size.equalTo(CGSize.init(width: 50, height: 50))
+            }
+            imageViewArray.append(imageView)
+            imageView.layer.cornerRadius = 25
+            imageView.clipsToBounds = true
+        }
+        let stackView = UIStackView.init(arrangedSubviews: imageViewArray)
+        // item间距
+        stackView.spacing = -15
+        // 水平方向布局
+        stackView.axis = .horizontal
+        // center对齐
+        stackView.alignment = .center
+        
+        stackView.distribution = .fillEqually
+        
+        
+        return stackView
+    }()
     
     private lazy var redView: UIView = {
         let redView = UIView()
@@ -38,47 +74,38 @@ class StackViewDemoViewController: CQBaseViewController {
         }
         return blueView
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         
-        stackView = UIStackView.init(arrangedSubviews: [redView, greenView, blueView])
         view.addSubview(stackView)
-        stackView.backgroundColor = .orange
-        // item间距
-        stackView.spacing = 30
-        // 水平方向布局
-        stackView.axis = .horizontal
-        // 底部对齐
-        stackView.alignment = .bottom
-        // 等间距
-        stackView.distribution = .equalSpacing
-        
         // 不设置宽度，让它宽度自适应
         stackView.snp.makeConstraints { (make) in
             make.top.equalTo(200)
             make.right.equalToSuperview().offset(-20)
             make.height.equalTo(50)
         }
+        
+        view.addSubview(stackView2)
+        stackView2.snp.makeConstraints { (make) in
+            make.top.equalTo(100)
+            make.right.equalToSuperview().offset(-20)
+            make.height.equalTo(50)
+        }
+        
     }
     
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
         // remove或者隐藏，stackView都会重新布局
-        stackView.removeArrangedSubview(greenView)
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) {
-            self.stackView.insertArrangedSubview(self.greenView, at: 1)
-        }
-        
-        //greenView.removeFromSuperview()
-        //greenView.isHidden = true
-        
+        greenView.isHidden = true
     }
-
-
+    
 }
+
+
+
 
 
