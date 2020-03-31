d1e8d4b00ec11f31e0486a328f197eedb9908d2d
caiqiang <>
2020-03-31 18:01:20 +0800

    add hero file

---
+++ iOS_Storage/Contents/三方框架/Hero/HeroViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HeroViewController.swift
+//  iOS_Storage
+//
+//  Created by caiqiang on 2020/3/31.
+//  Copyright © 2020 蔡强. All rights reserved.
+//
+
+import UIKit
+
+class HeroViewController: CQBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
