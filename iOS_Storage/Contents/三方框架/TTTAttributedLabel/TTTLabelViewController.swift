4d14a53ba085446a87a5d6c278e0522161d880c3
caiqiang <1351675135@qq.com>
2020-04-17 18:01:25 +0800

    ttlabel

---
+++ iOS_Storage/Contents/三方框架/TTTAttributedLabel/TTTLabelViewController.swift
@@ -0,0 +1,51 @@
+//
+//  TTTLabelViewController.swift
+//  iOS_Storage
+//
+//  Created by caiqiang on 2020/4/17.
+//  Copyright © 2020 蔡强. All rights reserved.
+//
+
+import UIKit
+import TTTAttributedLabel
+
+class TTTLabelViewController: CQBaseViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        let ttLabel1 = TTTAttributedLabel.init(frame: .zero)
+        view.addSubview(ttLabel1)
+        ttLabel1.text = "百度：好滴好滴好喝的"
+        ttLabel1.textColor = .black
+        ttLabel1.font = .systemFont(ofSize: 16)
+        ttLabel1.backgroundColor = .orange
+        ttLabel1.delegate = self
+        ttLabel1.snp.makeConstraints { (make) in
+            make.center.equalToSuperview()
+            make.height.equalTo(30)
+        }
+        
+        // 添加链接
+        ttLabel1.addLink(with: NSTextCheckingResult.linkCheckingResult(range: NSRange.init(location: 0, length: 2), url: URL.init(string: "https://www.baidu.com")!), attributes: [
+            NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 20),
+            NSAttributedString.Key.backgroundColor: UIColor.blue])
+        
+    }
+    
+    
+
+}
+
+
+extension TTTLabelViewController: TTTAttributedLabelDelegate {
+    
+    func attributedLabel(_ label: TTTAttributedLabel!, didSelectLinkWith url: URL!) {
+        print(url.absoluteString)
+    }
+    
+}
--- iOS_Storage/Contents/常用的/正则表达式/RegularExpressionViewController.swift
+++ iOS_Storage/Contents/常用的/正则表达式/RegularExpressionViewController.swift
@@ -28,8 +28,10 @@ class RegularExpressionViewController: CQBaseViewController {
         label.delegate = self
         label.font = UIFont.systemFont(ofSize: 13, weight: .regular)
         label.textColor = .black
-        label.text = "当前链接：[百度](https://www.baidu.com)"
         label.isUserInteractionEnabled = true
+        label.numberOfLines = 10
+        label.backgroundColor = .orange
+        label.text = "当前链接1：[百度](https://www.baidu.com),当前链接2：[百度](https://www.baidu.com),当前链接3：[百度](https://www.baidu.com)"
         return label
     }()
     
@@ -40,10 +42,22 @@ class RegularExpressionViewController: CQBaseViewController {
         button.addTarget(self, action: #selector(checkButtonClicked), for: .touchUpInside)
         return button
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        //let string = "当前链接：[百度](https://www.baidu.com),当前链接：[guge](https://www.baidu.com),当前链接：[百度](https://www.baidu.com)"
+        //let string = "当前链接：[百度](https://www.baidu.com)套你猴子"
+        let string = "[百度](https://www.baidu.com) dytd[百度](https://www.baidu.com)"
+        // 正则规则字符串
+        let pattern = "(\\[.+?\\]\\([^\\)]+?\\))|(<.+?>)"
+        let p2 = #"\[.+?\]\(.+?\)"#
+        // 正则规则
+        let regex = try? NSRegularExpression(pattern: pattern, options: NSRegularExpression.Options.allowCommentsAndWhitespace)
+        /// 进行正则匹配
+        let num = regex?.matches(in: string, options: .withoutAnchoringBounds, range: NSRange.init(location: 0, length: string.count))
+        print("匹配数：\(num)")
+        
         // Do any additional setup after loading the view.
         
         view.addSubview(label)
@@ -60,6 +74,7 @@ class RegularExpressionViewController: CQBaseViewController {
             make.centerX.equalToSuperview()
             make.top.equalTo(180)
             make.width.equalTo(300)
+            make.height.equalTo(80)
         }
         
         checkButton.snp.makeConstraints { (make) in
@@ -72,7 +87,8 @@ class RegularExpressionViewController: CQBaseViewController {
     
     @objc private func checkButtonClicked() {
         checkMarkDownURL(label: self.label)
-        checkTTLabel()
+        //checkTTLabel()
+        checkURL(label: ttLabel)
     }
     
     private func test1() {
@@ -109,8 +125,9 @@ class RegularExpressionViewController: CQBaseViewController {
         if let results = regex?.matches(in: string, options: [], range: NSRange(location: 0, length: string.count)), results.count != 0 {
             print(results)
             print("匹配成功")
+            var newString = string
             for result in results {
-                let url = (string as NSString).substring(with: result.range)
+                let url = (newString as NSString).substring(with: result.range)
                 print("对应链接:",url)
                 // 替换文本
                 
@@ -118,63 +135,171 @@ class RegularExpressionViewController: CQBaseViewController {
                 
                 let linkName = url.components(separatedBy: ["[", "]"])[1]
                 let linkURL = url.components(separatedBy: ["(", ")"])[1]
-                let newString = string.replacingOccurrences(of: url, with: linkName)
+                newString = string.replacingOccurrences(of: url, with: linkName)
                 
                 // 找到对应链接
                 let range = (newString as NSString).range(of: linkName)
+                
                 
                 let attributedString = NSMutableAttributedString.init(string: newString)
                 attributedString.addAttribute(NSAttributedString.Key.font, value: UIFont.boldSystemFont(ofSize: 14), range: range)
                 attributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.red, range: range)
                 attributedString.addAttribute(NSAttributedString.Key.link, value: linkURL, range: range)
+                //attributedString.addAttribute(NSAttributedString.Key.link, value: linkURL, range: range)
                 
                 self.label.attributedText = attributedString
+                
+            }
+        }
+    }
+    
+    // MARK: - 使用 TTTLabel
+    private func checkTTLabel() {
+        // 源
+        let string = "当前链接：[百度](https://www.baidu.com),当前链接：[guge](https://www.baidu.com),当前链接：[百度](https://www.baidu.com)"
+        //let string = "当前链接：[百度](https://www.baidu.com)套你猴子"
+        // 正则规则字符串
+        let pattern = "(\\[.+\\]\\([^\\)]+\\))|(<.+>)"
+        // 正则规则
+        let regex = try? NSRegularExpression(pattern: pattern, options: NSRegularExpression.Options.caseInsensitive)
+        /// 进行正则匹配
+        
+        let num = regex?.numberOfMatches(in: string, options: [], range: NSRange.init(location: 0, length: string.count))
+        print("匹配数：\(num)")
+        
+        if let results = regex?.matches(in: string, options: [], range: NSRange(location: 0, length: string.count)), results.count != 0 {
+            print(results)
+            print("匹配成功")
+            var newString = string
+            for result in results {
+                let url = (newString as NSString).substring(with: result.range)
+                print("对应链接:",url)
+                // 替换文本
+                
+                //guard let range = Range.init(result.range) else { return }
+                
+                let linkName = url.components(separatedBy: ["[", "]"])[1]
+                let linkURL = url.components(separatedBy: ["(", ")"])[1]
+                newString = newString.replacingOccurrences(of: url, with: linkName)
+                
+                // 找到对应链接
+                let range = (newString as NSString).range(of: linkName)
+                
+                self.ttLabel.setText(newString)
+                
+                //                let attributedString = NSMutableAttributedString.init(string: newString)
+                //                attributedString.addAttribute(NSAttributedString.Key.font, value: UIFont.boldSystemFont(ofSize: 14), range: range)
+                //                attributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.red, range: range)
+                //                attributedString.addAttribute(NSAttributedString.Key.link, value: linkURL, range: range)
+                
+                //self.ttLabel.attributedText = attributedString
+                self.ttLabel.addLink(with: NSTextCheckingResult.linkCheckingResult(range: range, url: URL.init(string: linkURL)!), attributes: [
+                    NSAttributedString.Key.backgroundColor: UIColor.red,
+                    NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16),
+                    NSAttributedString.Key.foregroundColor: UIColor.blue
+                ])
                 
                 //let urlName = url.components(separatedBy: Set.init(arrayLiteral: ""))
             }
         }
     }
     
-    private func checkTTLabel() {
+    // MARK: - 使用 TTTLabel
+    private func checkURL(label: TTTAttributedLabel) {
         // 源
-        let string = "当前链接：[百度](https://www.baidu.com)"
+        guard let string: String = label.text as? String else {
+            return
+        }
         // 正则规则字符串
-        let pattern = "(\\[.+\\]\\([^\\)]+\\))|(<.+>)"
+        let pattern = "(\\[.+?\\]\\([^\\)]+?\\))|(<.+?>)"
         // 正则规则
-        let regex = try? NSRegularExpression(pattern: pattern, options: [])
+        let regex = try? NSRegularExpression(pattern: pattern, options: NSRegularExpression.Options.caseInsensitive)
         /// 进行正则匹配
         if let results = regex?.matches(in: string, options: [], range: NSRange(location: 0, length: string.count)), results.count != 0 {
             print(results)
             print("匹配成功")
+            //var newString = string
+            
+            var ranges: [NSRange] = []
+            
+            var names: [String] = []
+            var links: [String] = []
+            var nameRanges: [NSRange] = []
+            
+            
+            
+            
             for result in results {
                 let url = (string as NSString).substring(with: result.range)
+                ranges.append(result.range)
                 print("对应链接:",url)
-                // 替换文本
-                
-                //guard let range = Range.init(result.range) else { return }
                 
                 let linkName = url.components(separatedBy: ["[", "]"])[1]
                 let linkURL = url.components(separatedBy: ["(", ")"])[1]
-                let newString = string.replacingOccurrences(of: url, with: linkName)
+            }
+            
+            let rr = results.reversed()
+            
+            
+            
+            
+            print(ranges)
+            
+            //ranges.reverse()
+            
+            let ran2 = ranges
+            
+            var currentString = string as NSString
+            //label.text = currentString
+            
+            
+            
+            ran2.forEach { (range) in
                 
-                // 找到对应链接
-                let range = (newString as NSString).range(of: linkName)
+                let url = currentString.substring(with: range)
                 
-//                let attributedString = NSMutableAttributedString.init(string: newString)
-//                attributedString.addAttribute(NSAttributedString.Key.font, value: UIFont.boldSystemFont(ofSize: 14), range: range)
-//                attributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.red, range: range)
-//                attributedString.addAttribute(NSAttributedString.Key.link, value: linkURL, range: range)
+                // 替换文本
+                let linkName = url.components(separatedBy: ["[", "]"])[1]
+                let linkURL = url.components(separatedBy: ["(", ")"])[1]
                 
-                //self.ttLabel.attributedText = attributedString
-                self.ttLabel.addLink(with: NSTextCheckingResult.linkCheckingResult(range: range, url: URL.init(string: linkURL)!), attributes: [
-                    NSAttributedString.Key.foregroundColor: UIColor.red,
-                    NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16)
+                
+                
+                currentString = currentString.replacingOccurrences(of: url, with: linkName, options: [], range: range) as NSString
+                let currentRange = NSRange.init(location: range.location, length: linkName.count)
+                
+                names.append(linkName)
+                links.append(linkURL)
+                nameRanges.append(currentRange)
+                
+            }
+            
+            
+            let attributedString = NSMutableAttributedString.init(string: currentString as String)
+            for (index, range) in nameRanges.enumerated() {
+                // 添加链接
+                label.addLink(with: NSTextCheckingResult.linkCheckingResult(range: range, url: URL.init(string: links[index])!), attributes: [
+                    NSAttributedString.Key.backgroundColor: UIColor.red,
+                    NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16),
+                    NSAttributedString.Key.foregroundColor: UIColor.blue
                 ])
                 
-                self.ttLabel.setText(newString)
                 
-                //let urlName = url.components(separatedBy: Set.init(arrayLiteral: ""))
+                attributedString.addAttribute(NSAttributedString.Key.font, value: UIFont.boldSystemFont(ofSize: 14), range: range)
+                attributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.red, range: range)
+                
+                
+                
+                //currentString = currentString.replacingOccurrences(of: url, with: linkName) as NSString
+                
+                
+                
+                // 找到对应链接
+                //let range = (newString as NSString).range(of: linkName)
+                
+                //label.setText(currentString)
+                label.attributedText = attributedString
             }
+            
         }
     }
     
