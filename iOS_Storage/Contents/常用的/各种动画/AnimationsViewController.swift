d6e2fdaf37d7362f70f01831811e001481ca8a16
caiqiang <1351675135@qq.com>
2020-04-28 17:43:30 +0800

    animation vc modify

--- iOS_Storage/Contents/常用的/各种动画/AnimationsViewController.swift
+++ iOS_Storage/Contents/常用的/各种动画/AnimationsViewController.swift
@@ -8,71 +8,16 @@
 
 import UIKit
 
-class AnimationsViewController: CQBaseViewController, UITableViewDataSource, UITableViewDelegate {
-    
-    private let dataArray: [Model] = [Model(title: "tableView动画", sel: #selector(gotoTableViewAnimationVC)),
-                                      Model(title: "UIView动画", sel: #selector(gotoUIViewAnimationVC))]
-    
-    private lazy var tableView: UITableView = {
-        let tableView = UITableView()
-        tableView.dataSource = self
-        tableView.delegate = self
-        return tableView
-    }()
+class AnimationsViewController: JumpListViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
         
-        view.addSubview(tableView)
-        tableView.snp.makeConstraints { (make) in
-            make.edges.equalToSuperview()
-        }
+        self.dataArray = [ItemModel(title: "tableView动画", targetVC: TableViewAnimationViewController()),
+                          ItemModel(title: "UIView动画", targetVC: UIViewAnimationViewController())]
         
     }
     
-    
-    // MARK: - action
-    
-    @objc private func gotoTableViewAnimationVC() {
-        let vc = TableViewAnimationViewController()
-        navigationController?.pushViewController(vc, animated: true)
-    }
-    
-    @objc private func gotoUIViewAnimationVC() {
-        let vc = UIViewAnimationViewController()
-        navigationController?.pushViewController(vc, animated: true)
-    }
-    
-    
-    // MARK: - UITableView DataSource & Delegate
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dataArray.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let reuseID = "reuseID"
-        var cell = tableView.dequeueReusableCell(withIdentifier: reuseID)
-        if cell == nil {
-            cell = UITableViewCell(style: .default, reuseIdentifier: reuseID)
-        }
-        cell?.textLabel?.text = dataArray[indexPath.row].title
-        return cell!
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let model = dataArray[indexPath.row]
-        perform(model.sel)
-    }
-    
-}
-
-
-// MARK: -
-
-fileprivate struct Model {
-    var title: String?
-    var sel: Selector?
 }
