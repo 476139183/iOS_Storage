57558b45259325a6259110608d73a5fee12eead9
caiqiang <1351675135@qq.com>
2020-07-02 10:50:09 +0800

    add base collectionView

---
+++ iOS_Storage/Contents/积累/UIKit相关/CollectionViewDemos/泛型collectionView/BaseCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  BaseCollectionViewCell.swift
+//  iOS_Storage
+//
+//  Created by caiqiang on 2020/7/1.
+//  Copyright © 2020 蔡强. All rights reserved.
+//
+
+import UIKit
+
+class BaseCollectionViewCell<M: Any>: UICollectionViewCell {
+    
+}
---
+++ iOS_Storage/Contents/积累/UIKit相关/CollectionViewDemos/泛型collectionView/BaseCollectionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BaseCollectionViewController.swift
+//  iOS_Storage
+//
+//  Created by caiqiang on 2020/7/1.
+//  Copyright © 2020 蔡强. All rights reserved.
+//
+
+import UIKit
+
+class BaseCollectionViewController<CM: Any, C: BaseCollectionViewCell<CM>>: CQBaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+}
