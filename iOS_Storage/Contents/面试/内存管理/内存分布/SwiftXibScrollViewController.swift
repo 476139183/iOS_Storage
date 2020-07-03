3c13fccfc9014ebcb272d38005e5deb7a1e58f23
caiqiang <1351675135@qq.com>
2020-07-03 08:52:03 +0800

    update

---
+++ iOS_Storage/Contents/面试/内存管理/内存分布/SwiftXibScrollViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftXibScrollViewController.swift
+//  iOS_Storage
+//
+//  Created by caiqiang on 2020/7/2.
+//  Copyright © 2020 蔡强. All rights reserved.
+//
+
+import UIKit
+
+class SwiftXibScrollViewController: CQBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
