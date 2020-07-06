cb28dcee6cbd69e8d5e73b2c9cad10f996c5b6d0
caiqiang <1351675135@qq.com>
2020-07-06 23:37:43 +0800

    use new build system

---
+++ iOS_Storage/Contents/积累/SwiftUI/List/SwiftUIListView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIListView.swift
+//  iOS_Storage
+//
+//  Created by caiqiang on 2020/7/6.
+//  Copyright © 2020 蔡强. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIListView: View {
+    var body: some View {
+        Text("H恩恩额d!")
+    }
+}
+
+struct SwiftUIListView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIListView()
+    }
+}
--- iOS_Storage/Contents/积累/SwiftUI/基础控件/SwiftUIBasicView.swift
+++ iOS_Storage/Contents/积累/SwiftUI/基础控件/SwiftUIBasicView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 
 struct SwiftUIBasicView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(systemName: "back")
+            Text("因缺思厅233, World!")
+        }
     }
 }
 
