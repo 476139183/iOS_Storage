5d0e2afca3b9f7d2ec50bb231bfcd4e65ddc203c
caiqiang <1351675135@qq.com>
2020-07-23 01:06:50 +0800

    update

---
+++ iOS_Storage/Contents/面试/收集/QuestionStorageViewController.swift
@@ -0,0 +1,19 @@
+//
+//  QuestionStorageViewController.swift
+//  iOS_Storage
+//
+//  Created by caiqiang on 2020/7/22.
+//  Copyright © 2020 蔡强. All rights reserved.
+//
+
+import UIKit
+
+class QuestionStorageViewController: JumpListViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
