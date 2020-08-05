ba6624186ba9d3e15aba9499dd3d8102faacae24
caiqiang <1351675135@qq.com>
2020-08-05 17:31:07 +0800

    update xib navibar

--- iOS_Storage/Contents/积累/xib/Swift/高度固定的导航栏/ConstantNaviView.swift
+++ iOS_Storage/Contents/积累/xib/Swift/高度固定的导航栏/ConstantNaviView.swift
@@ -14,7 +14,60 @@ class ConstantNaviView: UIView {
     
     /// 是否是默认高度
     @IBInspectable var isDefaultHeight: Bool = false
-
+    
+    @IBInspectable var title: String = "" {
+        didSet {
+            self.titleLabel.text = title
+        }
+    }
+    
+    @IBInspectable var isHideBackButton: Bool = false {
+        didSet {
+            self.backButton.isHidden = isHideBackButton
+        }
+    }
+    
+    private lazy var backButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage.init(named: "nav_back_icon"), for: .normal)
+        return button
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 18)
+        label.textColor = .black
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setupUI()
+    }
+    
+    private func setupUI() {
+        addSubview(titleLabel)
+        addSubview(backButton)
+        
+        titleLabel.snp.makeConstraints { (make) in
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(-10)
+        }
+        
+        backButton.snp.makeConstraints { (make) in
+            make.centerY.equalTo(titleLabel)
+            make.left.equalTo(20)
+            make.width.height.equalTo(30)
+        }
+    }
+    
     override func layoutSubviews() {
         super.layoutSubviews()
         
