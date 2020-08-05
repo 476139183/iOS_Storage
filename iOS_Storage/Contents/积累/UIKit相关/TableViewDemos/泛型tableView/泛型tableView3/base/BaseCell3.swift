3ca07c872cba14d835d0fffcceebccad680f05e9
caiqiang <1351675135@qq.com>
2020-08-05 11:07:11 +0800

    use common init

--- iOS_Storage/Contents/积累/UIKit相关/TableViewDemos/泛型tableView/泛型tableView3/base/BaseCell3.swift
+++ iOS_Storage/Contents/积累/UIKit相关/TableViewDemos/泛型tableView/泛型tableView3/base/BaseCell3.swift
@@ -40,14 +40,19 @@ class BaseCell3<M: Any>: UITableViewCell {
     func configUI() {
         
     }
+    
+    private func commonInit() {
+        configUI()
+    }
 
     required override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        configUI()
+        commonInit()
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
+        commonInit()
     }
     
 }
--- iOS_Storage/Contents/积累/UIKit相关/TableViewDemos/泛型tableView/泛型tableView3/base/BaseHeader3.swift
+++ iOS_Storage/Contents/积累/UIKit相关/TableViewDemos/泛型tableView/泛型tableView3/base/BaseHeader3.swift
@@ -26,14 +26,19 @@ class BaseHeader3<M: Any>: UITableViewHeaderFooterView {
     func configUI() {
         
     }
+    
+    private func commonInit() {
+        configUI()
+    }
 
     required override init(reuseIdentifier: String?) {
         super.init(reuseIdentifier: reuseIdentifier)
-        configUI()
+        commonInit()
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
+        commonInit()
     }
     
 }
