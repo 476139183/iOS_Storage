2723837d91c2c2593fbdf04500e711fee5a94c58
caiqiang <1351675135@qq.com>
2020-08-08 00:59:04 +0800

    update blog

--- iOS_Storage/Contents/积累/我的博客/MyBlogViewController.swift
+++ iOS_Storage/Contents/积累/我的博客/MyBlogViewController.swift
@@ -17,3 +17,14 @@ class MyBlogViewController: CQBaseViewController {
     }
 
 }
+
+// uid: 4212f351f6b5
+// 列表： https://www.jianshu.com/asimov/users/slug/4212f351f6b5/public_notes
+// 文章内容： https://www.jianshu.com/asimov/p/f936bb30f
+
+class BlogItemModel {
+    var title = ""
+    var slug = ""
+    var first_shared_at = ""
+    var list_image_url = ""
+}
