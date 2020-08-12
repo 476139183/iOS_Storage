4562ff087fe5d7a005dd2b46cdbb79e510299a6f
caiqiang <1351675135@qq.com>
2020-08-13 00:25:02 +0800

    update xib table

---
+++ iOS_Storage/Contents/积累/xib/TableView/SwiftXibCell.swift
@@ -0,0 +1,38 @@
+//
+//  SwiftXibCell.swift
+//  iOS_Storage
+//
+//  Created by caiqiang on 2020/8/11.
+//  Copyright © 2020 蔡强. All rights reserved.
+//
+
+import UIKit
+
+class SwiftXibCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        contentView.backgroundColor = .orange
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- iOS_Storage/Contents/积累/xib/TableView/SwiftXibTableViewController.swift
+++ iOS_Storage/Contents/积累/xib/TableView/SwiftXibTableViewController.swift
@@ -8,12 +8,50 @@
 
 import UIKit
 
-class SwiftXibTableViewController: CQBaseViewController {
+class SwiftXibTableViewController: CQBaseViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    private lazy var tableView: UITableView = {
+        let table = UITableView()
+        table.dataSource = self
+        table.delegate = self
+        //table.register(.init(nibName: SwiftXibCell.className, bundle: nil), forCellReuseIdentifier: SwiftXibCell.className)
+        table.rowHeight = 50
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { (make) in
+            make.top.equalTo(naviHeight)
+            make.left.right.bottom.equalToSuperview()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var cell = tableView.dequeueReusableCell(withIdentifier: SwiftXibCell.className)
+        
+        if let _ = Bundle.main.path(forResource: SwiftXibCell.className, ofType: "nib") {
+            if cell == nil {
+                cell = Bundle.main.loadNibNamed(SwiftXibCell.className, owner: nil, options: nil)?.first as? UITableViewCell
+            }
+        } else {
+            if cell == nil {
+                cell = Bundle.main.loadNibNamed(SwiftXibCell.className, owner: nil, options: nil)?.first as? UITableViewCell
+            }
+        }
+        
+        return cell!
     }
 
 }
+
+
