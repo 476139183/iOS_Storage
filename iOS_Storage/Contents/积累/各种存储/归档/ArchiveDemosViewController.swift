eb022f3608032848a5903f922a95e3c45a0e840a
caiqiang <1351675135@qq.com>
2020-08-17 23:46:38 +0800

    update archive

--- iOS_Storage/Contents/积累/各种存储/归档/ArchiveDemosViewController.swift
+++ iOS_Storage/Contents/积累/各种存储/归档/ArchiveDemosViewController.swift
@@ -14,17 +14,56 @@ class ArchiveDemosViewController: CQBaseViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let order1 = Order()
+        order1.orderName = "order1"
+        order1.userName = "Jack"
+        
+        // 归档
+        NSKeyedArchiver.archiveRootObject(order1, toFile: getSavePath())
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func getSavePath()->String {
+        let cacheDir:String = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true)[0]
+        let localPath:String = cacheDir.appending("TempTest.plist")
+        return localPath;
     }
-    */
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let order:Order = NSKeyedUnarchiver.unarchiveObject(withFile: getSavePath()) as! Order
+        print(order.orderName)
+        print(order.userName)
+    }
 
+}
+
+class Order: NSObject, NSCoding {
+    
+    var orderName:String = ""
+    var userName:String = ""
+
+//    init(orderName:String, userName:String) {
+//        self.orderName = orderName
+//        self.userName = userName
+//        super.init()
+//    }
+    
+    override init() {
+        super.init()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init()
+        self.orderName = aDecoder.decodeObject(forKey: "OrderName") as! String
+        self.userName = aDecoder.decodeObject(forKey: "UserName") as! String
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        
+        aCoder.encode(self.orderName, forKey:"OrderName")
+        
+        aCoder.encode(self.userName, forKey: "UserName")
+    }
+    
 }
