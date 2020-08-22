a8fc85fac83597ac9796219f70d125964bd98fe5
caiqiang <1351675135@qq.com>
2020-08-22 18:32:36 +0800

    add array xib

---
+++ iOS_Storage/Contents/积累/xib/添加数组属性/XibArrayPropertyView.swift
@@ -0,0 +1,16 @@
+//
+//  XibArrayPropertyView.swift
+//  iOS_Storage
+//
+//  Created by caiqiang on 2020/8/22.
+//  Copyright © 2020 蔡强. All rights reserved.
+//
+
+import UIKit
+
+class XibArrayPropertyView: UIView {
+
+    @IBInspectable var colors: [UIColor] = []
+    @IBInspectable var bgColor: UIColor = .white
+
+}
---
+++ iOS_Storage/Contents/积累/xib/添加数组属性/XibArrayPropertyViewController.swift
@@ -0,0 +1,19 @@
+//
+//  XibArrayPropertyViewController.swift
+//  iOS_Storage
+//
+//  Created by caiqiang on 2020/8/22.
+//  Copyright © 2020 蔡强. All rights reserved.
+//
+
+import UIKit
+
+class XibArrayPropertyViewController: CQBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
