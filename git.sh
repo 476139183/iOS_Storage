6375a742ab216dd7732b139e9403f29fd4599cb3
caiqiang <1351675135@qq.com>
2020-09-19 00:37:27 +0800

    update2

--- git.sh
+++ git.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # author:caiqiang
-read -p "请输入提交信息" commitInfo
+read -p "请输入提交信息:" commitInfo
 echo "提交中..."
 git add .
 git commit -m "$commitInfo"
 git push
-echo "提交完成"
+echo "提交完成!"
